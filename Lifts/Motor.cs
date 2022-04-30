5f0d93db9de95192ed28337d4ee056d816ed1279
mshcherba <mshcherba@ukr.net>
2022-04-30 20:21:09 +0300

    Added motor models with info

---
+++ Lifts/Motor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lifts
+{
+    class Motor
+    {
+
+        public int StopStatus = 0;
+
+        public void Up(ref int floor)
+        {
+            if (floor != Settings.getInstance().FLOORCOUNT && StopStatus == 0) floor += 1;
+        }
+
+
+        public void Down(ref int floor)
+        {
+            if (floor - 1 != -1 && StopStatus == 0) floor -= 1;
+        }
+
+        public void Stop()
+        {
+        }
+
+        public void StopOnFloor(ref bool Finished)
+        {
+            Finished = false;
+            if (StopStatus == 0) { StopStatus = 1; Console.WriteLine("Ліфт зупинився"); } //Зупинка
+            else if (StopStatus == 1) { StopStatus = 0; Console.WriteLine("Ліфт відкрив двері"); ; Finished = true; } // Відкриття дверей
+            //else if (StopStatus == 2) { StopStatus = 0; Console.WriteLine("Ліфт зачинив двері"); Finished = true; } //Закриття дверей
+        }
+    }
+}
