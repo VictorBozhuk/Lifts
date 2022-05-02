2385077bc13d702f6b2235727e7e090c81b6bcb4
nicoolle <nikakozmanko@gmail.com>
2022-05-02 17:35:44 +0200

    Added functionality for doors

---
+++ Lifts/Door.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lifts
+{
+    class Door
+    {
+        public StateDoor stateDoor = StateDoor.closed;
+        public void Open()
+        {
+            stateDoor = StateDoor.opened;
+        }
+
+        public void Close()
+        {
+            stateDoor = StateDoor.closed;
+        }
+
+        public bool IsOpen()
+        {
+            if (stateDoor == StateDoor.opened) return true;
+            else return false;
+        }
+    }
+
+    enum StateDoor
+    {
+        opened, closed
+    }
+}
